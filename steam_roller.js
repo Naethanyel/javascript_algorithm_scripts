10eb421bd7574814101a6275c16c7c8326d132c2
Naethanyel <naethanyelnowakowski@hotmail.com>
2017-06-08 21:23:01 -0400

    refactored to use recursion

--- steam_roller.js
+++ steam_roller.js
@@ -1,30 +1,17 @@
-//only works for 4 levels of nesting. needs recursion
-
 function steamrollArray(arr) {
   var steamRolled = [];
 
- arr.forEach(function (element) {
-    if(!Array.isArray(element)) {
-    steamRolled.push(element);
-    } else {
-      element.forEach(function (elem){
-        if(!Array.isArray(elem)) {
-        steamRolled.push(elem);
-        } else {
-          elem.forEach(function(e){
-            if(!Array.isArray(e)) {
-              steamRolled.push(e);
-            } else {
-              e.forEach(function(d){
-                steamRolled.push(d);
-              });
-            }
-          });
-        }
-      });
-    }
-  }); 
-  
+  var myFunc = function (array) {
+    array.forEach(function(element) {
+      if(!Array.isArray(element)) {
+        steamRolled.push(element);
+      } else {
+        myFunc(element);
+      }
+    });
+  };
+
+  myFunc(arr);
   return steamRolled;
 }
 
