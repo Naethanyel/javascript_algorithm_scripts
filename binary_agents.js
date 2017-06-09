f33eec402911ffd60a019165fd062da862d65b49
Naethanyel <naethanyelnowakowski@hotmail.com>
2017-06-08 21:50:30 -0400

    refactored

--- binary_agents.js
+++ binary_agents.js
@@ -1,4 +1,23 @@
 function binaryAgent(str) {
+
+    var binaryConverter = function (string) {
+
+    string = string.split(" "); // change to array
+    string = string.map(function(binaryCode){ //convert binary to character codes
+      return parseInt(binaryCode, 2);
+    });
+    string = string.map(function(charCode){ //convert character codes to characters
+      return String.fromCharCode(charCode);
+    });
+    return string.join(""); // convert to string 
+  };
+  
+  return binaryConverter(str);
+}
+
+
+ /* original version
+ 
   var binaryArray = str.split(" ");
   var digitArray = [];
   var charArray = [];
@@ -7,8 +26,6 @@ function binaryAgent(str) {
     var character = String.fromCharCode(digit);
     charArray.push(character);
   }
-  var sentence = charArray.join("");
-  return sentence;
-}
-
+  return = charArray.join("");
+  */
 binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
